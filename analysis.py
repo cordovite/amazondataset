19d6e04e96bf332a6d924729cd95a9a6ea399636
cordovite <122401564+cordovite@users.noreply.github.com>
2025-04-04 21:05:35 -0600

    creating github repo

--- analysis.py
+++ analysis.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 # Load the dataset
 df = pd.read_csv("amazon_reviews.csv")  # update this if the file name is different
-
+ 
 # Show first 5 rows
 print(df.head())
 import logging
