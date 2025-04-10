8b3e65399de9b75590e47c02c41f5d661ba9afc8
cordovite <122401564+cordovite@users.noreply.github.com>
2025-04-09 19:03:46 -0600

    pushed all remaining changes

--- analysis.py
+++ analysis.py
@@ -3,8 +3,17 @@ import pandas as pd
 # Load the dataset (make sure the file name matches exactly)
 df = pd.read_csv("Amazon_Unlocked_Mobile_Sample.csv")
 
-# Show the first 5 rows
-print(df.head())
+# print(df.head())
 
-# Check the columns and data types
-print(df.info())
+# print(df.info())
+
+# top_rated = df.groupby('Product Name').agg({
+#     'Rating': 'mean',
+#     'Reviews': 'count'
+# }).query('Reviews >= 10').sort_values(by='Rating', ascending=False)
+
+# print("\nTop 5 highest-rated phones (with 10+ reviews):")
+# print(top_rated.head(5))
+
+print(df['Rating'].value_counts())
+
